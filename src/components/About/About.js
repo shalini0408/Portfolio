59064deb62729514df145fef42cf5a134619b040
Shalini Maknoor <shalinimaknoor@Shalinis-MacBook-Air.local>
2025-02-22 18:54:39 -0500

    My ortfolio

---
+++ src/components/About/About.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { stackList } from "../../data/ProjectData";
+import {
+  Image,
+  Technologies,
+  Tech,
+  TechImg,
+  TechName,
+  ContactWrapper,
+} from "./AboutElements";
+import ScrollAnimation from "react-animate-on-scroll";
+function About() {
+  return (
+    <ContactWrapper id="about">
+      <div className="Container">
+        <div className="SectionTitle">About Me</div>
+        <div className="BigCard">
+          <div className="AboutBio">
+            <ScrollAnimation animateIn="fadeInLeft">
+            Hello! My name is <strong>Shalini Maknoor</strong>. 
+            I'm originally from India and pursuing my master's in George Mason University. 
+            </ScrollAnimation>
+
+            <Technologies>
+              {stackList.map((stack, index) => (
+                <ScrollAnimation animateIn="fadeInLeft" key={index}>
+                  <Tech key={index} className="tech">
+                    <TechImg src={stack.img} alt={stack.name} />
+                    <TechName>{stack.name}</TechName>
+                  </Tech>
+                </ScrollAnimation>
+              ))}
+            </Technologies>
+          </div>
+
+        </div>
+      </div>
+    </ContactWrapper>
+  );
+}
+
+export default About;
